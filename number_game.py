5c7a2d1d155244cfe937302c2c0cebfb7b84fa3a
idarkblue981 <150585644+idarkblue981@users.noreply.github.com>
2025-01-13 19:52:51 +0200

    Update number_game.py

--- number_game.py
+++ number_game.py
@@ -1,17 +1,16 @@
-def main():
-    number_game()
+from random import randint
 
-def number_game():
-    import random
+def main():
     while True:
         try:
             nr_user = int(input("Write a number from 1 to 10: "))
-            nr_program = random.randint(1, 10)
+            nr_program = randint(1, 10)
+
             if nr_user == nr_program:
                 print("You win!")
-            elif nr_user <= 0:
+            elif nr_user < 1:
                 print("The number is too small. Try again.")
-            elif nr_user >= 11:
+            elif nr_user > 10:
                 print("The number is too big. Try again.")
             else:
                 print(f"You lose. I was thinking about {nr_program}.")
